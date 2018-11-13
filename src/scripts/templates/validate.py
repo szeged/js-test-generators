c7ec8547bfd1285d202573616e048be5fccc7926
Csaba Repasi <repasics@inf.u-szeged.hu>
2018-11-13 10:38:59 +0100

    Add validate template

---
+++ src/scripts/templates/validate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+validate_numeric = '''
+function validate_numeric(test_value, expected_result, false_results) {
+    var radix = 2 + Math.abs(expected_result % 35);
+    assert(test_value === expected_result);
+    assert(test_value.toString(radix) === expected_result.toString(radix));
+    for (var i in false_results) {
+        radix = 2 + Math.abs(false_results[i] % 35);
+        assert(test_value !== false_results[i]);
+        assert(test_value.toString(radix) !== false_results[i].toString(radix));
+    }
+}
+'''
