ab2b56a43d104d514db55d25390ff2b54de474e0
Csaba Repasi <repasics@inf.u-szeged.hu>
2018-11-14 08:37:42 +0100

    Add test runner script

---
+++ src/scripts/colors.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+class _bcolors:
+    header = '\033[95m'
+    okblue = '\033[94m'
+    okgreen = '\033[92m'
+    warning = '\033[93m'
+    fail = '\033[91m'
+    endc = '\033[0m'
+    bold = '\033[1m'
+    underline = '\033[4m'
